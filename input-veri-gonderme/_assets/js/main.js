f48d0acc134953509ec9b779dfad65f8ea0c1646
ozayakbas <ozayakbas@gmail.com>
2020-01-09 17:20:25 +0300

    Add files via upload

---
+++ input-veri-gonderme/_assets/js/main.js
@@ -0,0 +1,31 @@
+
+// Jquery ve Js ile yazım şekilleri
+
+// 1. Yazım Şekli
+function pushData(){
+  $("#valSend1").html($('#val1').val());
+  $("#valSend2").html($('#val2').val());
+  $("#valSend3").html($('#val3').val());
+}
+
+// 2. Yazım Şekli
+  // function pushData(){
+
+  //   document.getElementById('valSend1').innerHTML = document.getElementById('val1').value;
+  //   document.getElementById('valSend2').innerHTML = document.getElementById('val2').value;
+  //   document.getElementById('valSend3').innerHTML = document.getElementById('val3').value;
+  // }
+
+// 3. Yazım Şekli
+  // function pushData(){
+  // var inputText;
+  
+  //   inputText = document.getElementById('val1').value;
+  //   document.getElementById('valSend1').innerHTML = inputText;
+
+  //   inputText = document.getElementById('val2').value;
+  //   document.getElementById('valSend2').innerHTML = inputText;
+
+  //   inputText = document.getElementById('val3').value;
+  //   document.getElementById('valSend3').innerHTML = inputText;
+  // }
